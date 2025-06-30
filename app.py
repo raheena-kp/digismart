21e5598aa77dd6bd77c546583b66b7689ec209c6
unknown <raheenakp01@gmail.com>
2025-06-30 22:00:05 +0300

    changed to digital marketing

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+from flask_mail import Mail, Message
+app = Flask(__name__)
+app.config['MAIL_SERVER']= 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'smart.dm.services@gmail.com'
+app.config['MAIL_PASSWORD'] = 'iwqpgbcscftrbjua'
+mail = Mail(app)
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/contact', methods=['POST'])
+def contact():
+    name = request.form['name']
+    email = request.form['email']
+    phone = request.form['phone']
+    message = request.form['message']
+    print(name, email, phone, message)
+    msg = Message(subject='New Contact Form Submission', sender=app.config['MAIL_USERNAME'], recipients=['smart.dm.services@gmail.com'], body=f"Name: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}")
+    mail.send(msg)
+    return "Thank you for contacting us!"
+if __name__ == '__main__': app.run(debug=True)
